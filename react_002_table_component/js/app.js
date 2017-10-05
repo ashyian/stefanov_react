606b72710d8ab6bed542172d5217ea1a940e7930
Aleksandr Shyian <al.vl.shiyan@gmail.com>
2017-10-05 14:19:06 +0300

    Adde a sort indication

--- react_002_table_component/js/app.js
+++ react_002_table_component/js/app.js
@@ -30,20 +30,29 @@ var Excel = React.createClass({
   },
 
   getInitialState: function() {
-    return {data: this.props.initialData};
+    return {
+      data: this.props.initialData,
+      sortby: null,
+      descending: false
+    };
   },
 
   // sorting column by clicking on head
   _sort: function(event) {
     var columnNumber = event.target.cellIndex;
     var dataCopy = this.state.data.slice();
+    var sortOrder = this.state.sortby === columnNumber && !this.state.descending
 
     dataCopy.sort(function(a, b) {
-      return a[columnNumber] > b[columnNumber] ? 1 : -1;
+      return sortOrder 
+        ? (a[columnNumber] < b[columnNumber] ? 1 : -1)
+        : (a[columnNumber] > b[columnNumber] ? 1 : -1);
     });
 
     this.setState({
-      data: dataCopy
+      data: dataCopy,
+      sortby: columnNumber,
+      descending: sortOrder
     });
   },
 
@@ -53,8 +62,11 @@ var Excel = React.createClass({
         React.DOM.thead({onClick: this._sort},
           React.DOM.tr(null,
             this.props.headers.map(function(title, idx) {
+              if (this.state.sortby === idx) {
+                title += this.state.descending ? "\u2191" : "\u2193";
+              }
               return React.DOM.th({key: idx}, title);
-            })
+            }, this)
           )
         ),
         React.DOM.tbody(null,
